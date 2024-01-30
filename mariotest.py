9e9ea408b5e9b2b51d4186fba5da7f01ae9d6138
FishByTheFin <zxfyer84derp@gmail.com>
2024-01-29 20:18:34 -0800

    initial commit

---
+++ mariotest.py
@@ -0,0 +1,52 @@
+# Import the game
+import gym_super_mario_bros
+# Import the Joypad wrapper
+from nes_py.wrappers import JoypadSpace
+# Import the SIMPLIFIED controls
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+# Import Frame Stacker Wrapper and GrayScaling Wrapper
+from gym.wrappers import GrayScaleObservation
+# Import Vectorization Wrappers
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+# Import Matplotlib to show the impact of frame stacking
+from matplotlib import pyplot as plt
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+# Create a flag - restart or not
+done = True
+# Loop through each frame in the game
+for step in range(100000): 
+    # Start the game to begin with 
+    if done: 
+        # Start the gamee
+        env.reset()
+    # Do random actions
+    state, reward, done, info = env.step(env.action_space.sample())
+    # Show the game on the screen
+    env.render()
+# Close the game
+env.close()
+
+# 1. Create the base environment
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+# 2. Simplify the controls 
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+# 3. Grayscale
+env = GrayScaleObservation(env, keep_dim=True)
+# 4. Wrap inside the Dummy Environment
+env = DummyVecEnv([lambda: env])
+# 5. Stack the frames
+env = VecFrameStack(env, 4, channels_order='last')
+
+state = env.reset()
+
+state, reward, done, info = env.step([5])
+
+plt.figure(figsize=(20,16))
+for idx in range(state.shape[3]):
+    plt.subplot(1,4,idx+1)
+    plt.imshow(state[0][:,:,idx])
+plt.show()

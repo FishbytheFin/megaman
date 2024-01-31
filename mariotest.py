0fe0f0f4b7484aa67ef8fd0f8e2e90b5cd6a2307
FishByTheFin <zxfyer84derp@gmail.com>
2024-01-31 12:35:04 -0800

    fix an error which resulted from not grabbing enough varibles from each environment step

--- mariotest.py
+++ mariotest.py
@@ -12,7 +12,18 @@ from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
 # Import Matplotlib to show the impact of frame stacking
 from matplotlib import pyplot as plt
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
+# Import os for file path management
+import os 
+# Import PPO for algos
+from stable_baselines3 import PPO
+# Import Base Callback for saving models
+from stable_baselines3.common.callbacks import BaseCallback
+
+#env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-v0',apply_api_compatibility=True,render_mode="human" )
+
+
+
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 # Create a flag - restart or not
@@ -24,7 +35,7 @@ for step in range(100000):
         # Start the gamee
         env.reset()
     # Do random actions
-    state, reward, done, info = env.step(env.action_space.sample())
+    state, reward, done, info, foo = env.step(env.action_space.sample())
     # Show the game on the screen
     env.render()
 # Close the game
@@ -43,10 +54,58 @@ env = VecFrameStack(env, 4, channels_order='last')
 
 state = env.reset()
 
-state, reward, done, info = env.step([5])
+state, reward, done, info, foo = env.step(env.action_space.sample())
 
 plt.figure(figsize=(20,16))
 for idx in range(state.shape[3]):
     plt.subplot(1,4,idx+1)
     plt.imshow(state[0][:,:,idx])
 plt.show()
+
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+    
+CHECKPOINT_DIR = './train/'
+LOG_DIR = './logs/'
+
+# Setup model saving callback
+callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+
+# This is the AI model started
+model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, 
+            n_steps=512) 
+
+# Train the AI model, this is where the AI model starts to learn
+model.learn(total_timesteps=1000000, callback=callback)
+
+model.save('thisisatestmodel')
+
+# Load model
+model = PPO.load('./train/best_model_1000000')
+
+state = env.reset()
+
+# Start the game 
+state = env.reset()
+# Loop through the game
+while True: 
+    
+    action, _ = model.predict(state)
+    #state, reward, done, info = env.step(action)
+    state, reward, done, info, foo = env.step(env.action_space.sample())
+    env.render()
